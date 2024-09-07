295fc53e213a476cd11caf3d587aa907867edf7f
BOX-MIN <emmetschell@gmail.com>
2024-09-07 12:39:07 -0400

    Licence update

--- LevelEditor/LE_main.py
+++ LevelEditor/LE_main.py
@@ -1,3 +1,24 @@
+# PhysicsEngineAndLevelEditor is a 2D physics engine and level editor
+# in python, using pygame and pymunk, and rendering through openGL
+
+# Copyright (C) 2024  Emmet Schell
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# this program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# To contact the author of this program, Email them at
+# emmetschell@gmail.com.
+
 import json
 import ast
 from math import sqrt
